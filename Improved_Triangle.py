c24dfd61c1ddcef6c2d22dd2f1b2b9b1fcae50f5
ekocibelli <54696646+ekocibelli@users.noreply.github.com>
2020-02-04 20:40:00 -0500

    Add files via upload

---
+++ Improved_Triangle.py
@@ -0,0 +1,44 @@
+"""
+Created on Tuesday, Feb 04 2020
+Author: Ejona Kocibelli
+Project Description: Improved classifyTriangle function
+"""
+
+
+def classifyTriangle(a, b, c):
+
+    try:
+        """Parameters should be numbers, raises an exception if not so"""
+        a = float(a)
+        b = float(b)
+        c = float(c)
+    except ValueError:
+        return 'InvalidInput'
+    else:
+        [a, b, c] = sorted([a, b, c])
+        if a <= 0 or b <= 0 or c <= 0 or a > 200 or b > 200 or c > 200:
+            return 'InvalidInput'
+        else:
+
+            if (a + b > c) and (b + c > a) and (a + c > b):  # sum of two sides should be bigger than the third side
+                if round(((a ** 2) + (b ** 2)), 2) == round((c ** 2), 2):
+                    if a == b or b == c or c == a:  # check if triangle is right and isosceles
+                        return 'Right and Isosceles Triangle'
+                    elif a != b and b != c and a != c:  # check if triangle is right and scalene
+                        return 'Right and Scalene Triangle'
+                elif a == b == c:
+                    return "Equilateral"  # check if triangle is equilateral
+                elif a == b or b == c or c == a:
+                    return "Isosceles"  # check if triangle is isosceles
+                else:
+                    return "Scalene"  # otherwise, triange is a scalene
+            else:
+                return 'NotATriangle'  # it is not a triangle if sides are <= 0
+
+
+def main():
+    print(classifyTriangle(200, 200, 201))
+
+
+if __name__ == '__main__':
+    main()
